26ccbd6f872f1d4bafb8462f546438db676d07d0
Knivier <104603427+knivier@users.noreply.github.com>
2024-06-11 18:38:48 -0400

    Update ActiverseSound.java

--- ActiverseSound.java
+++ ActiverseSound.java
@@ -2,11 +2,23 @@ import javax.sound.sampled.*;
 import java.io.File;
 import java.io.IOException;
 
+/**
+ * Represents a sound player for playing audio files.
+ * This class provides methods to load, play, and stop audio files.
+ *
+ * @author Knivier
+ */
 public class ActiverseSound {
     private Clip clip;
 
+    /**
+     * Constructs a new ActiverseSound object with the specified audio file.
+     *
+     * @param filename The path to the audio file.
+     */
     public ActiverseSound(String filename) {
         try {
+            // Load the audio file
             AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(filename));
             clip = AudioSystem.getClip();
             clip.open(audioInputStream);
@@ -15,15 +27,23 @@ public class ActiverseSound {
         }
     }
 
+    /**
+     * Plays the loaded audio file.
+     */
     public void play() {
         if (clip != null) {
+            // Set the frame position to the beginning and start playing
             clip.setFramePosition(0);
             clip.start();
         }
     }
 
+    /**
+     * Stops the currently playing audio file.
+     */
     public void stop() {
         if (clip != null) {
+            // Stop playing
             clip.stop();
         }
     }
