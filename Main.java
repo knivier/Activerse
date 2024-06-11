f86e4dc53dfed366b5aba917296eb421b98bdcd7
Knivier <104603427+knivier@users.noreply.github.com>
2024-06-11 18:40:01 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,8 @@
+/**
+ * Instantiates a new myWorld class and starts the game engine
+ * 
+ * @author Knivier
+ */
 public class Main {
     public static void main(String[] args) {
         Activerse.start(new MyWorld());
