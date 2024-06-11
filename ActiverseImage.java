ce2f9a732bb8cebb31bb68476f13d6d42f684c10
Knivier <104603427+knivier@users.noreply.github.com>
2024-06-11 18:38:12 -0400

    Update ActiverseImage.java

--- ActiverseImage.java
+++ ActiverseImage.java
@@ -1,13 +1,30 @@
 import java.awt.Image;
 import java.awt.Toolkit;
 
+/**
+ * Represents an image loader for loading images from files.
+ * This class provides methods to load an image and retrieve it.
+ *
+ * @author Knivier
+ */
 public class ActiverseImage {
     private Image image;
 
+    /**
+     * Constructs a new ActiverseImage object with the image loaded from the specified file.
+     *
+     * @param filename The path to the image file.
+     */
     public ActiverseImage(String filename) {
+        // Load the image from the specified file
         image = Toolkit.getDefaultToolkit().getImage(filename);
     }
 
+    /**
+     * Gets the loaded image.
+     *
+     * @return The Image object representing the loaded image.
+     */
     public Image getImage() {
         return image;
     }
