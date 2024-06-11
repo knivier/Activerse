97a705ef3b5577a6cf3c21617a1b997cbaf9b3b1
Knivier <104603427+knivier@users.noreply.github.com>
2024-06-11 18:40:20 -0400

    Update MyActor.java

--- MyActor.java
+++ MyActor.java
@@ -1,10 +1,16 @@
+/**
+ * The base actor that extends the main Actor and can do more things
+ * 
+ * @author Knivier
+ */
 public class MyActor extends Actor {
-    public MyActor() {
+    public MyActor() { //creates new MyActor Object
         setImage(new ActiverseImage("actor.png"));
     }
 
     @Override
     public void act() {
         // Define behavior here
+        move(2);
     }
 }
