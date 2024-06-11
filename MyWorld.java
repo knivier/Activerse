be7bb4bdc981868be71dbb732d79605f94edd1a6
Knivier <104603427+knivier@users.noreply.github.com>
2024-06-11 18:40:34 -0400

    Update MyWorld.java

--- MyWorld.java
+++ MyWorld.java
@@ -1,6 +1,20 @@
+/**
+ * Represents a custom world for the application.
+ * This class extends the World class to customize the world's initialization.
+ *
+ * @author Knivier
+ */
 public class MyWorld extends World {
+    
+    /**
+     * Constructs a new instance of MyWorld with specific dimensions and cell size.
+     * Initializes the world and adds a MyActor object to it.
+     */
     public MyWorld() {
+        // Call the constructor of the superclass (World) with specified dimensions and cell size
         super(600, 400, 1);
+        
+        // Add a MyActor object to the world at the specified location (100, 100)
         addObject(new MyActor(), 100, 100);
     }
 }
