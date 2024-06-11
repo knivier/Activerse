6c2b2530c0e443eb6ee5a7b0589fbb2d7a14ec64
Knivier <104603427+knivier@users.noreply.github.com>
2024-06-11 18:39:23 -0400

    Update CollisionManager.java

--- CollisionManager.java
+++ CollisionManager.java
@@ -1,9 +1,26 @@
 import java.awt.Rectangle;
 
+/**
+ * Manages collision detection between actors in the world.
+ * This class provides a method to check if two actors intersect.
+ *
+ * @author Knivier
+ */
 public class CollisionManager {
+    
+    /**
+     * Checks if two actors intersect.
+     *
+     * @param a The first actor.
+     * @param b The second actor.
+     * @return true if the bounding boxes of the two actors intersect, false otherwise.
+     */
     public static boolean intersects(Actor a, Actor b) {
+        // Create bounding boxes for the two actors
         Rectangle r1 = new Rectangle(a.getX(), a.getY(), a.getImage().getImage().getWidth(null), a.getImage().getImage().getHeight(null));
         Rectangle r2 = new Rectangle(b.getX(), b.getY(), b.getImage().getImage().getWidth(null), b.getImage().getImage().getHeight(null));
+        
+        // Check if the bounding boxes intersect
         return r1.intersects(r2);
     }
 }
