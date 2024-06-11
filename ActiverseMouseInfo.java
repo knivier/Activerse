9724d7e913c0b0769c89228f3872cf051613d22f
Knivier <104603427+knivier@users.noreply.github.com>
2024-06-11 18:38:28 -0400

    Update ActiverseMouseInfo.java

--- ActiverseMouseInfo.java
+++ ActiverseMouseInfo.java
@@ -1,8 +1,20 @@
 import java.awt.Point;
 import java.awt.MouseInfo;
 
+/**
+ * Provides utility methods to retrieve information about the mouse pointer location.
+ *
+ * @author Knivier
+ */
 public class ActiverseMouseInfo {
+    
+    /**
+     * Retrieves the current location of the mouse pointer on the screen.
+     *
+     * @return A Point object representing the coordinates of the mouse pointer.
+     */
     public static Point getMouseLocation() {
+        // Get the current location of the mouse pointer using MouseInfo
         return MouseInfo.getPointerInfo().getLocation();
     }
 }
