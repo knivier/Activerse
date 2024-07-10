505d3ba7104dee6e9349425c33b73a4dfa633ae2
Kniev <agnivag.16@gmail.com>
2024-07-10 12:16:03 -0400

    removed some optimizations in Actor due to collisions not functioning properly

--- ActiverseEngine/Actor.java
+++ ActiverseEngine/Actor.java
@@ -8,13 +8,12 @@ import java.awt.geom.AffineTransform;
  * The base class for all actors in the world.
  */
 public abstract class Actor {
-    protected double direction;
+    protected double direction; // Changed access to protected
     private int x, y;
     private World world;
     private ActiverseImage image;
     private double velocityX, velocityY;
-    private int width, height;
-    private Rectangle boundingBox = new Rectangle();
+    private int height; // Added height field
 
     /**
      * Performs the actor's action.
@@ -42,30 +41,12 @@ public abstract class Actor {
     }
 
     /**
-     * Sets the x-coordinate of the actor's location.
-     *
-     * @param x The x-coordinate to set.
-     */
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    /**
      * Gets the y-coordinate of the actor's location.
      *
      * @return The y-coordinate of the actor's location.
      */
     public int getY() {
         return y;
-    }
-
-    /**
-     * Sets the y-coordinate of the actor's location.
-     *
-     * @param y The y-coordinate to set.
-     */
-    public void setY(int y) {
-        this.y = y;
     }
 
     /**
@@ -84,8 +65,6 @@ public abstract class Actor {
      */
     public void setImage(ActiverseImage image) {
         this.image = image;
-        this.width = image.getImage().getWidth(null);
-        this.height = image.getImage().getHeight(null);
     }
 
     /**
@@ -96,10 +75,14 @@ public abstract class Actor {
     public void paint(Graphics g) {
         if (image != null) {
             Graphics2D g2d = (Graphics2D) g;
+            int width = image.getImage().getWidth(null);
+            int height = image.getImage().getHeight(null);
 
             AffineTransform old = g2d.getTransform();
-            g2d.rotate(direction, x + (double) width / 2, y + (double) height / 2);
+
+            g2d.rotate(direction, x + width / 2, y + height / 2);
             g2d.drawImage(image.getImage(), x, y, null);
+
             g2d.setTransform(old);
         }
     }
@@ -140,8 +123,7 @@ public abstract class Actor {
      * @return The bounding box of the actor.
      */
     protected Rectangle getBoundingBox() {
-        boundingBox.setBounds(x, y, width, height);
-        return boundingBox;
+        return new Rectangle(x, y, getImage().getImage().getWidth(null), getImage().getImage().getHeight(null));
     }
 
     /**
@@ -155,8 +137,8 @@ public abstract class Actor {
         int newX = x + dx;
         int newY = y + dy;
         // Check if the new position exceeds the world limits
-        if (newX >= 0 && newX + width <= world.getWidth() &&
-                newY >= 0 && newY + height <= world.getHeight()) {
+        if (newX >= 0 && newX + getImage().getImage().getWidth(null) <= world.getWidth() &&
+                newY >= 0 && newY + getImage().getImage().getHeight(null) <= world.getHeight()) {
             x = newX;
             y = newY;
         }
@@ -251,4 +233,22 @@ public abstract class Actor {
     public void setHeight(int height) {
         this.height = height;
     }
+    /**
+     * Sets the x-coordinate of the actor's location.
+     *
+     * @param x The x-coordinate to set.
+     */
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    /**
+     * Sets the y-coordinate of the actor's location.
+     *
+     * @param y The y-coordinate to set.
+     */
+    public void setY(int y) {
+        this.y = y;
+    }
+
 }
