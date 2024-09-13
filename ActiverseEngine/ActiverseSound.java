2faf9ceb45aa687671facf11b614a6bb05531669
Kniev <agnivag.16@gmail.com>
2024-09-13 17:54:11 -0400

    Added more detailed error messages

--- ActiverseEngine/ActiverseSound.java
+++ ActiverseEngine/ActiverseSound.java
@@ -28,6 +28,10 @@ public class ActiverseSound {
             clip = AudioSystem.getClip();
             clip.open(audioInputStream);
         } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            System.out.println("An unidentified exception ocurred while loading the audio file. Please see the stack trace for more information.");
+            System.out.println("Filename: " + filename + " threw your exception. Please check the file path or verify it is valid. .WAV files in limited length are supported.");
+            System.out.println("Contact ActiverseEngine support for bugs.");
+            System.out.println("Printing stack trace now");
             e.printStackTrace();
         }
     }
@@ -39,6 +43,10 @@ public class ActiverseSound {
         if (clip != null) {
             clip.setFramePosition(0);
             clip.start();
+        }
+        else if(clip == null){
+            System.out.println("The audio file is null. Please check the file path or verify it is valid. .WAV files in limited length are supported.");
+            System.out.println("Filename of null audio file: " + filename);
         }
     }
 
