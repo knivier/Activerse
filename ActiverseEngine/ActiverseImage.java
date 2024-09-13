090f61f957379926aa48fdc41d9298782c79b69f
Kniev <agnivag.16@gmail.com>
2024-09-13 18:04:26 -0400

    added some comments in params

--- ActiverseEngine/ActiverseImage.java
+++ ActiverseEngine/ActiverseImage.java
@@ -10,13 +10,13 @@ import java.awt.*;
  * @version 1.2.2
  */
 public class ActiverseImage {
-    private final Image image;
+    private final Image image; // Image object to store the loaded image
     private final String path; // Store the image path
 
     /**
      * Constructs a new ActiverseImage object with the image loaded from the specified file.
      *
-     * @param filename The path to the image file.
+     * @param filename The path to the image file via (jpg/jpeg, png, gif, bmp, wbmp)
      */
     public ActiverseImage(String filename) {
         image = Toolkit.getDefaultToolkit().getImage(filename);
