c4312fc19c13dfef2e2e9e1c9633878deb347490
Knivier <agnivag.16@gmail.com>
2024-11-02 17:09:21 -0400

    simple error handling that compares with ACESH standard

--- ActiverseEngine/World.java
+++ ActiverseEngine/World.java
@@ -133,8 +133,8 @@ public class World extends JPanel implements ActionListener, KeyListener {
                 System.err.println("Activerse.properties not found or could not be loaded.");
             }
         } catch (IOException e) {
+            System.out.println("10A.IN:(LN: loadProperties() - ACEHS Error thrown; an error occurred while loading properties. Default values will be used. Contact ActiverseEngine support for bugs. Otherwise, please provide a properties file.");
             e.printStackTrace();
-            System.err.println("Error loading Activerse.properties: " + e.getMessage());
         }
         return props;
     }
