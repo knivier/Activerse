b6f19faefffe6328bba85a4442434db5fe4c0e8c
Knivier <agnivag.16@gmail.com>
2025-01-22 18:12:16 -0500

    comments

--- BreakoutWorld.java
+++ BreakoutWorld.java
@@ -9,15 +9,15 @@ public class BreakoutWorld extends World {
           // Creates a new ball object at position 300,500
         addObject(new Paddle(), 300,500); // Adds the paddle to bat the ball
         addObject(new Ball(300,500), 300, 500); // Adds the ball to the world
-        addObject(new Brick(), 20 ,50 );
+        addObject(new Brick(), 20 ,50 ); // Adding 3 bricks to the world
         addObject(new Brick(), 200 ,50 );
         addObject(new Brick(), 500 ,50 );
     }
 
     public void update(){
         super.update();
-        if(getActors().stream().noneMatch(actor -> actor instanceof Brick)){
-            showText(150, 150, "You  win!");
+        if(getActors().stream().noneMatch(actor -> actor instanceof Brick)){ // Checking for any instances of brick; let's not hard code this 
+            showText(150, 150, "You  win!"); 
             javax.swing.JOptionPane.showMessageDialog(null, "You won!", "Game Over", javax.swing.JOptionPane.INFORMATION_MESSAGE);
             System.exit(0);
         }
