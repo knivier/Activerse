04ece5650e9ce0e7e0f5c5f1d101443c95efa07d
Knivier <agnivag.16@gmail.com>
2025-05-30 21:07:01 -0400

    RMd out

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-
+import ActiverseEngine.Activerse;
 /**
  * Instantiates a new myWorld class and starts the game engine
  *
@@ -11,6 +11,6 @@ public class Main {
      * @param args console arguments, none are valid, all are not
      */
     public static void main(String[] args) {
-        //Activerse.start(new WORLDTYPEHERE()); // Starts breakout
+        Activerse.start(new SandBox()); // Starts sandbox world
     }
 }
